5af81f9917c3e53249842934564641f1e634fcfe
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-22 13:30:27 -0600

    Update port used to be either env variable or 3000

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+// Specify what port app will be listening on
+
 const app = require("./app");
 const { PORT } = require("./config");
+
+// PORT will be retrieved from environment variable if specified or 3000
 
 app.listen(PORT, function() {
     console.log(`Server has started on port ${PORT}`);
