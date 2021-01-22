879dd000aecae3136b482616d8b06899032ce832
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-21 22:10:50 -0600

    Add user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,11 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const ExpressError = require("../helpers/expressError");
+const partialUpdate = require("../helpers/partialUpdate.js");
+const { user } = require("../db.js");
 
 class User {
-    // static methods
+    // getAll => returns array of all users with username, first_name, last_name, and email
     static async getAll() {
         const userResults = await db.query(
             `SELECT username, first_name, last_name, email FROM users)`
@@ -14,6 +16,7 @@ class User {
         return users;
     }
 
+    // getOne => returns instance of one user with all data except for password
     static async getOne(username) {
         const userResult = await db.query(
             `SELECT username, first_name, last_name, email, created_at, airline, favorite_layover
@@ -26,6 +29,7 @@ class User {
         return userResult.rows[0];
     }
 
+    // create => creates new user. Requires values for username, password, first_name, last_name, email and is_admin
     static async create(newUserObj) {
         // inserts user into database and returns token
         const { username, password, first_name, last_name, email, is_admin } = newUserObj;
@@ -43,12 +47,29 @@ class User {
         return token;
     }
 
-    // static async update(username, userData) {}
+    // updateUser => updates user. userData can have any parameters that are desired to be updated
+    static async updateUser(username, userData) {
+        const { query, values } = partialUpdate("users", userData, "username", username);
 
-    // static async delete(username) {}
+        const results = await db.query(query, values);
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Could not find user with username ${username}`, 404);
+        }
+        return results.rows[0];
+    }
 
-    // methods to like commment, activity, and photo??
+    // delete => deletes user instance in database. Returns 404 if no such user with given username
+    static async delete(username) {
+        const results = await db.query(
+            `DELETE FROM users WHERE username = $1 RETURNING username`, [username]
+        )
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Could not find user with username ${username}`)
+        }
+        return results.rows[0];
+    }
 
+    // authenticate => returns token if correct password and username. Throws 404 if cant find user, throws 500 if unable to authenticate
     static async authenticate(username, password) {
         const userResults = await db.query(`SELECT * FROM users WHERE username=$1`, [username]);
         const user = userResults[0];
