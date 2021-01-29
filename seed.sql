610f5b147640ba66861070106085c9793d91f06f
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-01-29 11:39:25 -0600

    Change layover_codes to be lowercase

--- seed.sql
+++ seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO layovers(layover_code, city_name, country_name, description, currency, international, main_img_url, thumbnail_url)
 VALUES 
-('LIM', 'Lima', 'Peru', 'Capital of Peru', 'SOL', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
-('CDG', 'Paris', 'France', 'City of love', 'EUR', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
-('LHR', 'London', 'United Kingdom', 'God Save the Queen', 'GBP', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
-('GIG', 'Rio De Janeiro', 'Brazil', 'Bom Dia', 'BRL', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
-('YVR', 'Vancouver', 'Canada', 'Eh?', 'CAD', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
-('NRT', 'Narita', 'Japan', 'Ohayouu', 'JPY', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250');
+('lim', 'Lima', 'Peru', 'Capital of Peru', 'sol', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
+('cdg', 'Paris', 'France', 'City of love', 'eur', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
+('lhr', 'London', 'United Kingdom', 'God Save the Queen', 'gbp', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
+('gig', 'Rio De Janeiro', 'Brazil', 'Bom Dia', 'brl', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
+('yvr', 'Vancouver', 'Canada', 'Eh?', 'cad', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250'),
+('nrt', 'Narita', 'Japan', 'Ohayouu', 'jpy', TRUE, 'http://placekitten.com/350/250', 'http://placekitten.com/350/250');
 
