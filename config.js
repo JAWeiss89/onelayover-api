7ded059fc0e475f5a17c664e88224eb6aada3bb5
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-03-09 11:46:25 -0600

    Update config file with env variables

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "abc123"; // retrieve key from deployed env variable or "abc123" if running on local
 
-const PORT = process.env.PORT || 3000; // if running on local machine, use port 3000
+const PORT = +process.env.PORT || 3000; // if running on local machine, use port 3000
 
 let DB_URI;
 if (process.env.NODE_ENV === "test") { // if intesting environment,
     DB_URI = "onelayover_test"; // use test database called "onelayover-test"
 } else {
     // otherwise use database specified in deployed env variable or local app database called "onelayover"
-    DB_URI = process.send.DATABASE_URL || "onelayover"; 
+    DB_URI = process.env.DATABASE_URL || "onelayover"; 
 }
 
 module.exports = {
